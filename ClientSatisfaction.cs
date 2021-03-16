98ac69c4edc339607512341379b6fdcc8ddd6fe8
Sibaxolise <sibaxolise2000@gmail.com>
2021-03-16 22:43:22 +0200

    Added ClientSatisfaction Class

---
+++ ClientSatisfaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCenterProgram
+{
+    class ClientSatisfaction
+    {
+        public void GetReport()
+        {
+
+
+        }
+
+        public void GetClientRecords()
+        {
+
+
+        }
+
+        public void TimeStramps()
+        {
+
+
+        }
+
+        public bool CompareRecords()
+        {
+
+            return true;
+        }
+
+        public void AddClientFollowuP()
+        {
+
+
+        }
+
+        public void GetFeedBack()
+        {
+
+
+        }
+    }
+}
