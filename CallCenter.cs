f1ec8e1f4d141cfbe1baf55bb663d6470bf373ce
Tingerhedgehog <Tingerhedgehog351@gmail.com>
2021-03-17 18:06:41 +0200

    Added callCenter overrides

--- CallCenter.cs
+++ CallCenter.cs
@@ -86,6 +86,19 @@ namespace CallCenterProgram
         }
 
         //overrides
+        public override string ToString()
+        {
+            return base.ToString();
+        }
 
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
