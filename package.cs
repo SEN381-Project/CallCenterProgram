362f05c5855926670a8c8de8180d344e769c710a
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-03-20 22:43:55 +0200

    Implementing Contract Maintenance Classes -- Revised

---
+++ package.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCenterProgram
+{
+    class Package
+    {
+        private string packageName;
+
+        public string PackageName { get => packageName; set => packageName = value; }
+
+        public void CreatePackage(List<Service> services, List<ServiceLevel> serviceLevels)
+        {
+
+        }
+        public void ViewPerformance(int contractTypeId)
+        {
+
+        }
+    }
+}
---
+++ service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCenterProgram
+{
+    class Service
+    {
+        private string serviceName;
+        private int serviceId;
+        private string workExpenses;
+        private string equipmentType;
+        private bool state; 
+
+        public string ServiceName { get => serviceName; set => serviceName = value; }
+        public int ServiceId { get => serviceId; set => serviceId = value; }
+        public string WorkExpenses { get => workExpenses; set => workExpenses = value; }
+        public string EquipmentType { get => equipmentType; set => equipmentType = value; }
+        public bool State { get => state; set => state = value; }
+
+        public void AddService()
+        {
+
+        }
+
+        public void ChangeState()
+        {
+            // CHange the state of a service from active to active (vice versa)
+        }
+    }
+}
