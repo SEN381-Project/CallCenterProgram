9ddf56ba60a20d0cb3e12861eac84ed662c0fdbd
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-03-21 22:18:57 +0200

    Implementing Contract Maintenance Classes (Issues Resolved)

---
+++ Contract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCenterProgram
+{
+    class Contract
+    {
+        private bool onGoing;
+
+        public bool OnGoing { get => onGoing; set => onGoing = value; }
+
+        public void CreateContract(Package package, int clientID)
+        {
+
+        }
+
+        public void TerminateContract()
+        {
+            // switch onGoing from true to false to indicate termination.
+        }
+    }
+}
