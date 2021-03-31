3e48f4478e090749375da4338e067dfbd8bbf591
Tingerhedgehog <Tingerhedgehog351@gmail.com>
2021-03-31 14:24:59 +0200

    Revert "added list"
    This reverts commit 686052902a3590bb22615f4ed5abe527cd2b877a.

--- Bussiness_Logic/Call.cs
+++ Bussiness_Logic/Call.cs
@@ -35,40 +35,27 @@ namespace CallCenterProgram
         }
         public void RecordCallStats()
         {
-            
+
         }
         public DateTime CreateTimestamp()
         {
             DateTime timeStamp = DateTime.Now;
             return timeStamp;
         }
-        public List<string> GenerateCallReport()
+        public void GenerateCallReport()
         {
-            List<string> reports = new List<string>();
 
-            //call report
-            reports.Add(Console.ReadLine());
-            //problem Info
-            reports.Add(Console.ReadLine());
-            //work Request
-            reports.Add(Console.ReadLine());
-
-
-            //return
-            return reports;
         }
         public void InsertCallIntoDB()
         {
-            //lists
-            List<string> reports = GenerateCallReport();
-            //vars
+            float callTime = 0f;
             string faultReport = faultInfo.ReportFault();
-            string callReport = reports[0];
-            string problemInfo = reports[1];
-            string workRequest = reports[2];
+            string callReport = "";
+            string problemInfo = "";
+            string workRequest = "";
             int callDuration = 0;
 
-            CallDB.InsertCall(initialTimeStamp, finalTimeStamp, faultReport, callReport, problemInfo, workRequest, callDuration);
+            CallDB.InsertCall(callTime, initialTimeStamp, finalTimeStamp, faultReport,callReport,problemInfo,workRequest,callDuration);
         }
     }
 }
