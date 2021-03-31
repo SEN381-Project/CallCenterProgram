fed177d350df37141c11423512ba43c1a5e0d093
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-03-31 10:45:22 +0200

    Adding Contract Maintenance Data handeler class

---
+++ Data_Access/ContractMaintenanceAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CallCenterProgram
+{
+    class ContractMaintenanceAccess
+    {
+        string connect = "Data Source=.; Initial Catalog= CallCenterDatabase; Integrated Security= SSPI";
+        SqlConnection conn;
+        SqlCommand command;
+        SqlDataReader reader;
+
+    }
+}
