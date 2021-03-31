6c9cefc99a046011dbb855fe7eca8a304eeaf948
Tingerhedgehog <Tingerhedgehog351@gmail.com>
2021-03-31 13:43:53 +0200

    Linked data access to logic for call class

--- Bussiness_Logic/Call.cs
+++ Bussiness_Logic/Call.cs
@@ -8,38 +8,54 @@ namespace CallCenterProgram
 {
     class Call : CallStats
     {
+        //classes
+        call_DataAccess CallDB = new call_DataAccess();
+        FaultInfo faultInfo = new FaultInfo();
+
+        //vars
+        private DateTime initialTimeStamp;
+        private DateTime finalTimeStamp;
+        //methods
         public void MakeCall()
         {
-
+            initialTimeStamp = CreateTimestamp();
         }
 
         public void TakeCall()
         {
-
+            initialTimeStamp = CreateTimestamp();
         }
         public void EndCall()
         {
-
-        }
-        public void InsertCallIntoDB()
-        {
-
+            finalTimeStamp = CreateTimestamp();
         }
         public void CrossReference()
         {
-
+            Console.WriteLine("Added call to old system.");
         }
         public void RecordCallStats()
         {
 
         }
-        public void CreateTimestamp()
+        public DateTime CreateTimestamp()
         {
-
+            DateTime timeStamp = DateTime.Now;
+            return timeStamp;
         }
         public void GenerateCallReport()
         {
 
+        }
+        public void InsertCallIntoDB()
+        {
+            float callTime = 0f;
+            string faultReport = faultInfo.ReportFault();
+            string callReport = "";
+            string problemInfo = "";
+            string workRequest = "";
+            int callDuration = 0;
+
+            CallDB.InsertCall(callTime, initialTimeStamp, finalTimeStamp, faultReport,callReport,problemInfo,workRequest,callDuration);
         }
     }
 }
