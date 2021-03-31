136dd6369cd98c2c32b9bf1c4218f777879a8b12
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-03-31 10:37:18 +0200

    Adding package and client ids to Contract class

--- Contract.cs
+++ Contract.cs
@@ -9,8 +9,12 @@ namespace CallCenterProgram
     class Contract
     {
         private bool onGoing;
+        private int packageID;
+        private int clientID;
 
         public bool OnGoing { get => onGoing; set => onGoing = value; }
+        public int PackageID { get => packageID; set => packageID = value; }
+        public int ClientID { get => clientID; set => clientID = value; }
 
         public void CreateContract(Package package, int clientID)
         {
