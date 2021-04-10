22b7ec1876b2f51c9bacdf89a35c029bea596e83
SP <devandebeer2000@gmail.com>
2021-04-10 12:43:29 +0200

    Added business clients form

---
+++ Business Client Info.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace CallCenterProgram
+{
+    partial class Business_Client_Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvBusinessClients = new System.Windows.Forms.DataGridView();
+            this.btnClientMainForm = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnViewIndividualClients = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBusinessClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Business Clients";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dgvBusinessClients
+            // 
+            this.dgvBusinessClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBusinessClients.Location = new System.Drawing.Point(29, 48);
+            this.dgvBusinessClients.Name = "dgvBusinessClients";
+            this.dgvBusinessClients.Size = new System.Drawing.Size(561, 200);
+            this.dgvBusinessClients.TabIndex = 1;
+            // 
+            // btnClientMainForm
+            // 
+            this.btnClientMainForm.Location = new System.Drawing.Point(515, 19);
+            this.btnClientMainForm.Name = "btnClientMainForm";
+            this.btnClientMainForm.Size = new System.Drawing.Size(75, 23);
+            this.btnClientMainForm.TabIndex = 2;
+            this.btnClientMainForm.Text = "Back";
+            this.btnClientMainForm.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(29, 254);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnViewIndividualClients
+            // 
+            this.btnViewIndividualClients.Location = new System.Drawing.Point(454, 254);
+            this.btnViewIndividualClients.Name = "btnViewIndividualClients";
+            this.btnViewIndividualClients.Size = new System.Drawing.Size(136, 23);
+            this.btnViewIndividualClients.TabIndex = 4;
+            this.btnViewIndividualClients.Text = "View Individual Clients";
+            this.btnViewIndividualClients.UseVisualStyleBackColor = true;
+            // 
+            // Business_Client_Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(624, 450);
+            this.Controls.Add(this.btnViewIndividualClients);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnClientMainForm);
+            this.Controls.Add(this.dgvBusinessClients);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(112)))), ((int)(((byte)(233)))));
+            this.Name = "Business_Client_Info";
+            this.Text = "Business_Client_Info";
+            this.Load += new System.EventHandler(this.Business_Client_Info_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBusinessClients)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvBusinessClients;
+        private System.Windows.Forms.Button btnClientMainForm;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnViewIndividualClients;
+    }
+}
