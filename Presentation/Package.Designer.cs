e5be9d0f124b06f5a01c616756930f631d9b2156
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-04-15 16:52:16 +0200

    Update Package UI to help solve database list issue

--- Presentation/Package.Designer.cs
+++ Presentation/Package.Designer.cs
@@ -29,6 +29,7 @@ namespace CallCenterProgram.Presentation
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Package));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -45,6 +46,7 @@ namespace CallCenterProgram.Presentation
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.label11 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -54,7 +56,7 @@ namespace CallCenterProgram.Presentation
             // button1
             // 
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(112)))), ((int)(((byte)(233)))));
-            this.button1.Location = new System.Drawing.Point(639, 580);
+            this.button1.Location = new System.Drawing.Point(649, 695);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 48);
             this.button1.TabIndex = 46;
@@ -63,21 +65,21 @@ namespace CallCenterProgram.Presentation
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(345, 527);
+            this.textBox3.Location = new System.Drawing.Point(355, 642);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(441, 22);
             this.textBox3.TabIndex = 44;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(345, 465);
+            this.textBox2.Location = new System.Drawing.Point(355, 580);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(441, 22);
             this.textBox2.TabIndex = 43;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(345, 416);
+            this.textBox1.Location = new System.Drawing.Point(355, 531);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(441, 22);
             this.textBox1.TabIndex = 42;
@@ -86,9 +88,9 @@ namespace CallCenterProgram.Presentation
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(24, 528);
+            this.label5.Location = new System.Drawing.Point(34, 643);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(231, 26);
+            this.label5.Size = new System.Drawing.Size(185, 21);
             this.label5.TabIndex = 39;
             this.label5.Text = "Enter Package Name:";
             // 
@@ -96,29 +98,29 @@ namespace CallCenterProgram.Presentation
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(24, 464);
+            this.label4.Location = new System.Drawing.Point(34, 579);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(260, 53);
+            this.label4.Size = new System.Drawing.Size(216, 42);
             this.label4.TabIndex = 38;
-            this.label4.Text = "Enter Service Level ID: \r\n(first table on the right) ";
+            this.label4.Text = "Enter Service Level IDs: \r\n(first table on the right) ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(24, 417);
+            this.label3.Location = new System.Drawing.Point(34, 532);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(386, 26);
+            this.label3.Size = new System.Drawing.Size(317, 21);
             this.label3.TabIndex = 37;
-            this.label3.Text = "Enter Service ID (from table above):";
+            this.label3.Text = "Enter Service IDs (from table above):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 370);
+            this.label2.Location = new System.Drawing.Point(26, 354);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(879, 31);
+            this.label2.Size = new System.Drawing.Size(703, 25);
             this.label2.TabIndex = 36;
             this.label2.Text = "ADD PACKAGE SECTION: (ALL FIELDS TO BE FILLED IN!)";
             // 
@@ -200,19 +202,29 @@ namespace CallCenterProgram.Presentation
             // button3
             // 
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(112)))), ((int)(((byte)(233)))));
-            this.button3.Location = new System.Drawing.Point(28, 580);
+            this.button3.Location = new System.Drawing.Point(38, 695);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(147, 48);
             this.button3.TabIndex = 60;
             this.button3.Text = "EXIT";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(658, 119);
+            this.label6.TabIndex = 61;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
             // Package
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
-            this.ClientSize = new System.Drawing.Size(1773, 684);
+            this.ClientSize = new System.Drawing.Size(1773, 760);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.label11);
@@ -255,5 +267,6 @@ namespace CallCenterProgram.Presentation
         private System.Windows.Forms.DataGridView dataGridView3;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
     }
 }
