454ac9cab3e9825b5e83fdfd5337e92480a4a86f
SP <devandebeer2000@gmail.com>
2021-04-22 12:28:11 +0200

    updated resources

--- Presentation/Individual Client Info.cs
+++ Presentation/Individual Client Info.cs
@@ -25,5 +25,19 @@ namespace CallCenterProgram.Presentation
             BackColor = Color.FromArgb(26, 26, 26);
             ForeColor = Color.FromArgb(102, 112, 233);
         }
+
+        private void btnViewBusinessClients_Click(object sender, EventArgs e)
+        {
+            Business_Client_Info businessform = new Business_Client_Info();
+            businessform.Show();
+            this.Close();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            Client_Home homeform = new Client_Home();
+            homeform.Show();
+            this.Close();
+        }
     }
 }
